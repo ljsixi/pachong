36cd45dea47c316a5a59392cc3f75c035d6e4dab
ljsixi <953143907@qq.com>
2017-11-22 15:25:51 +0800

    aaaa

---
+++ pachong/src/pachong/TTest3.java
@@ -0,0 +1,9 @@
+package pachong;
+public class TTest3 {  
+    
+    public static void main(String[] args) throws Exception {  
+        System.out.println("--start--");  
+        Test3.getNetworkImage("http://www.tooopen.com/img/87.aspx", "E://downloadImage//");  
+        System.out.println("--end--");  
+    }  
+}  
